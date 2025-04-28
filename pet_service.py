7356859a19ce4f3c32737e80fe084b0758c9f8b8
Khushi227 <91377607+Khushi227@users.noreply.github.com>
2025-04-28 01:14:33 +0000

    small tweak

--- pet_service.py
+++ pet_service.py
@@ -94,42 +94,6 @@ def get_db():
     finally:
         db.close()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")  #  Endpoint for login
-
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    """
-    Retrieves the current user from a JWT token.
-
-    Args:
-        token (str, optional): The JWT token from the Authorization header. Defaults to Depends(oauth2_scheme).
-        db (Session, optional): The database session. Defaults to Depends(get_db).
-
-    Returns:
-        UserDB: The UserDB object representing the current user.
-
-    Raises:
-        HTTPException:
-            - 401: If the token is invalid or expired.
-            - 401: If the user is not found in the database.
-    """
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")  # "sub" claim contains the username
-        if username is None:
-            raise credentials_exception
-        user = db.query(UserDB).filter(UserDB.username == username).first()
-        if user is None:
-            raise credentials_exception
-        return user
-    except JWTError:
-        raise credentials_exception
-
-
 # Helper function to validate the JWT token
 def validate_token(token: str):
     try:
